a5f94d5625f4a47e00535d495f798c28f6b09b7b
Parnia-prhn <87763496+Parnia-prhn@users.noreply.github.com>
2021-08-20 19:27:03 +0430

    Update CLK_DV.vhd

--- CLK_DV.vhd
+++ CLK_DV.vhd
@@ -1,43 +1,35 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08/20/2021 05:56:16 PM
--- Design Name: 
--- Module Name: CLK_DV - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+
+
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity CLK_DV is
---  Port ( );
+port(
+    clk_50: in std_logic;
+    clk_1s: out std_logic
+    );
 end CLK_DV;
 
 architecture Behavioral of CLK_DV is
+signal counter:integer range 0 to 25000:=0;
+signal t: std_logic := '0';
 
 begin
-
+ process(clk_50)
+ begin
+  if(rising_edge(clk_50)) then
+   counter <= counter + 1;
+   if(counter=25000) then 
+   t <= not t;
+    counter <= 0;
+    
+   end if;
+  end if;
+ end process;
+ clk_1s <= t;
 
 end Behavioral;
+
